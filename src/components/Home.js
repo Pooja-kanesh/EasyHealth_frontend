3631d860461a06a9cfee95cf3851f15fdcb76029
anuradhakaneshgmail <129434577+anuradhakaneshgmail@users.noreply.github.com>
2023-03-31 00:36:58 +0530

    Added desc to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,22 +23,27 @@ const Home = function () {
             <div className="container-box">
                 <div className="row row-0">
                     <div className="col-8 description book">
-                        <h2><b>Book Your Slot</b></h2>Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cum aliquid animi fugiat modi impedit ratione ipsa odit reiciendis,
-                        ipsam delectus nemo. Ducimus ullam voluptatum facilis non vitae veritatis porro laboriosam.</div>
+                        <h2><b>Book Your Slot</b></h2>To ease the process of identifying Vaccination Centres 
+                        in your city, We have launched EasyHealth where you can check the
+                         availability of vaccines such as BCG, Hepatitis B, Polio, MMR, RotaVirus etc. 
+                         You can also see vaccine registration slots available for each type of vaccine 
+                         In order to get vaccinated you will have to first register yourself to 
+                         receive a specific dose of vaccine by doing a self registration.</div>
                     <div className="col-4 pic"><img src={slot_book} alt="slot booking" /></div>
                 </div>
                 <div className="row row-0">
                     <div className="col-4 pic"><img src={child_vac} alt="child vaccination" /></div>
-                    <div className="col-8 description child-vac"><h2><b>Schedule Child's Vaccination</b></h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cum aliquid animi fugiat modi impedit ratione ipsa odit reiciendis,
-                        ipsam delectus nemo. Ducimus ullam voluptatum facilis non vitae veritatis porro laboriosam</div>
+                    <div className="col-8 description child-vac"><h2><b>Schedule Child's Vaccination</b></h2>
+                    Vaccines are most effective when they are administered to children at the right age and with 
+                    the recommended dosage as children are susceptible to certain diseases at certain ages.
+                    Register your child's vaccine and get updates for dosage</div>
                 </div>
                 <div className="row row-0">
-                    <div className="col-8 description record"><h2><b>Keep Health Records</b></h2>Lorem  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cum aliquid animi fugiat modi impedit ratione ipsa odit reiciendis,
-                        ipsam delectus nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        ipsam delectus nemo. Ducimus ullam voluptatum facilis non vitae veritatis porro laboriosam.</div>
+                    <div className="col-8 description record"><h2><b>Keep Health Records</b></h2>
+                    Health record is a confidential compilation of pertinent facts of an individual's health history, 
+                    including all past and present medical conditions, illnesses and treatments, with emphasis on the 
+                    specific events affecting the patient during the current episode of care.
+                    We provide health record for all our patients for smooth health tracking </div>
                     <div className="col-4 pic"><img src={health} alt="health records" /></div>
                 </div>
             </div>
